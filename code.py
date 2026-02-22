f946929bc9df74c9e72515fe2f6d82d0e1963aef
Harivansh Mehta <harivanshmehta4@gmail.com>
2026-02-22 15:18:46 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,5 +1,7 @@
 import yfinance as yf
 largeCap = yf.download("POWERGRID.NS", start = "2025-03-04", end = "2026-03-04", interval = "1d")
-print(largeCap.head()["Close"])
+print("Large Cap Stock Analysis: POWERGRID (NSE)")
+print(largeCap.describe()["Close"])
 smallCap = yf.download("CYIENTDLM.NS", start = "2025-03-04", end = "2026-03-04", interval = "1d")
-print(smallCap.head()["Close"])
+print("Small Cap Stock Analysis: CYIENTDLM (NSE)")
+print(smallCap.describe()["Close"])
